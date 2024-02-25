7dd0f157670a31f07552a5e8707d3abb925c6fd9
Majid Ash <Magid.Ash.Si@gmail.com>
2024-02-25 13:26:00 -0800

    Scoring players

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const choices = ["rock", "paper", "scissors"];
 
-const plyerChoice = prompt("Choose rock, paper, scissors");
+let playerSocre = 0;
+let computerSocre = 0;
 
 const checkWinner = (player, computer) => {
   if (player === computer) {
@@ -17,27 +18,46 @@ const checkWinner = (player, computer) => {
 const showResult = (result) => {
   if (result === "player") {
     console.log("You win.");
+    playerSocre++;
   } else if (result === "computer") {
     console.log("You lose!");
+    computerSocre++;
   } else {
     console.log("It's a tie.");
   }
+
+  console.log(`Your score: ${playerSocre}`);
+  console.log(`Computer's score: ${computerSocre}`);
+  console.log(".............................");
 };
 
 const play = () => {
-  if (choices.indexOf(plyerChoice?.toLowerCase()) !== -1) {
-    console.log(`You choose ${plyerChoice.toLowerCase()}`);
+  if (playerSocre === 2 || computerSocre === 2) {
+    if (playerSocre === 2) {
+      console.log("You win overall.");
+      return;
+    } else if (computerSocre === 2) {
+      console.log("You lost overall!");
+      return;
+    }
   } else {
-    console.log("You cheated");
-    return;
+    const plyerChoice = prompt("Choose rock, paper, scissors");
+
+    if (choices.indexOf(plyerChoice?.toLowerCase()) !== -1) {
+      console.log(`You choose ${plyerChoice.toLowerCase()}`);
+    } else {
+      console.log("You cheated");
+      return;
+    }
+
+    const randomNumber = Math.floor(Math.random() * choices.length);
+    const computerChoise = choices[randomNumber];
+    console.log(`Computer chooses: ${computerChoise}`);
+
+    const gameResult = checkWinner(plyerChoice, computerChoise);
+    showResult(gameResult);
+    play();
   }
-
-  const randomNumber = Math.floor(Math.random() * choices.length);
-  const computerChoise = choices[randomNumber];
-  console.log(`Computer chooses: ${computerChoise}`);
-
-  const gameResult = checkWinner(plyerChoice, computerChoise);
-  showResult(gameResult);
 };
 
 play();
