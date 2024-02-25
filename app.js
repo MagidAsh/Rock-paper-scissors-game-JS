3e68bac51ff009476c3ad49bedd0c68ec8546b21
Majid Ash <Magid.Ash.Si@gmail.com>
2024-02-25 12:22:50 -0800

    Refactoring and cleaning the codes

--- app.js
+++ app.js
@@ -1,55 +1,33 @@
-// step1 : Getting selected values from user and computer
-
 const choices = ["rock", "paper", "scissors"];
 
 const plyerChoice = prompt("Choose rock, paper, scissors");
 
-// const computerValue = prompt("Choose rock, paper, scissors").toLowerCase();
-
-// if (plyerChoice === "" || plyerChoice === null) {
-//   console.log("You cheated");
-// } else {
-//   console.log(`You choose ${plyerChoice}`);
-// }
-
-// چون مقدار نال و استرینگ خالی یک مقدار فالزی است میتوان شرط را اینگونه نوشت
-// if (!plyerChoice) {
-//   console.log("You cheated");
-// } else {
-//   console.log(`You choose ${plyerChoice}`);
-// }
-
-// یا اینکه شرط بالا را برعکس کرد
-if (plyerChoice) {
-  console.log(`You choose ${plyerChoice}`);
-} else {
-  console.log("You cheated");
-}
-
-const randomNumber = Math.floor(Math.random() * choices.length);
-const computerChoise = choices[randomNumber];
-console.log(`Computer chooses: ${computerChoise}`);
-
-// step2 : Determine the winner of the game
-
-if (plyerChoice === computerChoise) {
-  console.log("It's a tie.");
-} else if (plyerChoice === "rock") {
-  if (computerChoise === "scissors") {
-    console.log("You win.");
+const checkWinner = (player, computer) => {
+  if (player === computer) {
+    return "equal";
+  } else if (player === "rock") {
+    return computer === "scissors" ? "player" : "computer";
+  } else if (player === "paper") {
+    return computer === "rock" ? "player" : "computer";
   } else {
-    console.log("You lose!");
+    return computer === "paper" ? "player" : "computer";
   }
-} else if (plyerChoice === "paper") {
-  if (computerChoise === "rock") {
-    console.log("You win.");
+};
+
+const play = () => {
+  if (choices.indexOf(plyerChoice.toLowerCase()) !== -1) {
+    console.log(`You choose ${plyerChoice.toLowerCase()}`);
   } else {
-    console.log("You lose!");
+    console.log("You cheated");
+    return;
   }
-} else {
-  if (computerChoise === "paper") {
-    console.log("You win.");
-  } else {
-    console.log("You lose!");
-  }
-}
+
+  const randomNumber = Math.floor(Math.random() * choices.length);
+  const computerChoise = choices[randomNumber];
+  console.log(`Computer chooses: ${computerChoise}`);
+
+  const result = checkWinner(plyerChoice, computerChoise);
+  console.log(result);
+};
+
+play();
